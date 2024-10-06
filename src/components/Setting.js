10c7377714f647319d46063edb28922d3c36cd75
asqre <amitanand.asqre@gmail.com>
2024-10-06 12:33:33 +0530

    updates

--- src/components/Setting.js
+++ src/components/Setting.js
@@ -4,7 +4,7 @@ const Settings = () => {
   return (
     <div className="mt-8">
       <h3 className="text-xl font-bold mb-2">Settings</h3>
-      {/* Puzzle and game settings will be implemented here */}
+ 
     </div>
   );
 };
